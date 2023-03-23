ba7561b04c781e31664a1a0b10a393a11fe0b1ff
Fatima Ali <fofoalmali@gmail.com>
2023-03-23 17:12:13 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// code your solution here
+//Define a Function Using Function Declaration
+function saturdayFun(activity="roller-skate"){
+    
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun();
+saturdayFun("play football");
+
+//Define Function Expression
+let mondayWork=function(activity="go to the office"){
+    return `This Monday, I will ${activity}.`;
+};
+mondayWork();
+mondayWork("go to park");
+
+//wrapAdjective
+function wrapAdjective(outerString="*") {
+    const innerFunction = function (innerString="*") {
+      return `You are ${outerString}${innerString}${outerString}!`;
+    };
+    return innerFunction;
+}
+wrapAdjective();
+wrapAdjective("%")("a dedicated programmer"); 
